39fa8c5f7b68fd70156661db4650a5ae794a5f8d
Patryk Danych <patryk.danych@gmail.com>
2022-01-27 21:45:16 +0100

    Dodaj pliki projektów.

---
+++ TaxiApp/Main.cpp
@@ -0,0 +1,20 @@
+#include "Login.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+
+int Main(array<String^>^ args)
+
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	TaxiApp::Login form;
+	Application::Run(% form);
+	return 0;
+
+}
+
+
+
